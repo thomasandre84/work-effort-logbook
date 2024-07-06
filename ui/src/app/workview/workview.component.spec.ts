47fa2a2ed4f834293f5c77637bcfa05f3eed5160
Thomas Andre <thomasandre84@gmail.com>
2024-07-06 09:56:27 +0200

    add workview

---
+++ ui/src/app/workview/workview.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkviewComponent } from './workview.component';
+
+describe('WorkviewComponent', () => {
+  let component: WorkviewComponent;
+  let fixture: ComponentFixture<WorkviewComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [WorkviewComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WorkviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ui/src/app/workview/workview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workview',
+  standalone: true,
+  imports: [],
+  templateUrl: './workview.component.html',
+  styleUrl: './workview.component.scss'
+})
+export class WorkviewComponent {
+
+}
